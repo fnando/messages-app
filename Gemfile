6e7296498255385ed9b154c1dbfae714099fd297
Nando Vieira <fnando.vieira@gmail.com>
2015-10-08 18:30:36 -0300

    Add metrics.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,6 @@ ruby "2.2.3"
 
 gem "sinatra"
 gem "puma"
+gem "aitch"
+gem "sucker_punch"
+gem "dotenv"
---
+++ lib/google_analytics.rb
@@ -0,0 +1,30 @@
+class GoogleAnalytics
+  def self.page_view(path, request)
+    host = request.host
+    ua = request.user_agent
+    referrer = request.referer
+    remote_ip = request.ip
+
+    Job.new.async.perform(host, path, ua, referrer, remote_ip)
+  end
+
+  class Job
+    include SuckerPunch::Job
+
+    def perform(host, path, ua, referrer, remote_ip)
+      params = {
+        v: ENV["GOOGLE_ANALYTICS_VERSION"],
+        tid: ENV["GOOGLE_ANALYTICS_CODE"],
+        cid: 555,
+        t: "pageview",
+        dh: host,
+        dp: path,
+        ua: ua,
+        dr: referrer,
+        uip: remote_ip,
+      }
+
+      Aitch.post("http://www.google-analytics.com/collect", params)
+    end
+  end
+end
