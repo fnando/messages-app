af24c51929cd33efcbcdcfc592c3b5e09c015830
Nando Vieira <fnando.vieira@gmail.com>
2015-10-09 00:11:28 -0300

    Skip tracking.

--- server.rb
+++ server.rb
@@ -36,21 +36,21 @@ get "/" do
 end
 
 get "/success.svg" do
-  GoogleAnalytics.page_view("/success.svg", request)
+  GoogleAnalytics.page_view("/success.svg", request) unless params[:skip]
   send_image(type: "check", stroke: "#B5CFB5", fill: "#DFF0D8", text: "#326B31", icon: "#417505")
 end
 
 get "/info.svg" do
-  GoogleAnalytics.page_view("/info.svg", request)
+  GoogleAnalytics.page_view("/info.svg", request) unless params[:skip]
   send_image(type: "info", stroke: "#B5CAE5", fill: "#E0E9F4", text: "#3060A3", icon: "#3060A3")
 end
 
 get "/warning.svg" do
-  GoogleAnalytics.page_view("/warning.svg", request)
+  GoogleAnalytics.page_view("/warning.svg", request) unless params[:skip]
   send_image(type: "warning", stroke: "#E8D7B1", fill: "#FCF8E3", text: "#8D6A32", icon: "#8D6A32")
 end
 
 get "/error.svg" do
-  GoogleAnalytics.page_view("/error.svg", request)
+  GoogleAnalytics.page_view("/error.svg", request) unless params[:skip]
   send_image(type: "error", stroke: "#E9B4B4", fill: "#F5DEDE", text: "#A93538", icon: "#A93538")
 end
